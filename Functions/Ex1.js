2f8b7cd0609e4fe8ded72b51320626a021732d39
mcoria <martin.coria@bibamgroup.com>
2018-08-28 16:44:10 -0300

    modificacion ideas y funciones

--- Functions/Ex1.js
+++ Functions/Ex1.js
@@ -4,3 +4,17 @@
 // Mostrar el resultado en pantalla como: " tu perro tiene NN años "
 
 var prompt = require('prompt-sync')()
+
+var numero1 = 3
+var numero2 = 5
+
+function suma_y_resta() {
+    resultado = numero1 + numero2
+    console.log('El resultado es ' + resultado)
+}
+
+
+suma_y_resta()
+
+
+
