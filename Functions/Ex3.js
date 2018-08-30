2fe2d966c991df655bed25e56b506e514dbe6ba9
Martin Coria <coriam82@gmail.com>
2018-08-30 07:36:36 -0300

    funciones

---
+++ Functions/Ex3.js
@@ -0,0 +1,16 @@
+// Convertidor de temperatura de grados a Fahreheit y viceversa
+
+var prompt = require('prompt-sync')()
+
+console.log('1 Grados a Fahrenheit')
+console.log('2 Fahrenheit a Grados')
+
+var input = prompt('Ingrese una opcion: ')
+
+    if (input == '1') {
+       var numCtoF = prompt('Ingrese un numero para convertir de C a F: ')
+}   else if (input3 = '2') {
+       var numFtoC = prompt('Ingrese un numero para convertir de F a C: ')
+}
+
+// armar bien la funcion
