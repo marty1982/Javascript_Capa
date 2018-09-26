9bbe71c54a682b16da047718ee1ef6ef2a75c158
mcoria <martin.coria@bibamgroup.com>
2018-09-26 11:53:24 -0300

    cambios

--- API/Ex1.js
+++ API/Ex1.js
@@ -2,8 +2,9 @@
 
 var request = require('sync-request');
 
-var res = request('GET', 'http://navi.api.prd.devtrip.com.ar/flights-api/v1/flights/roundtrips?clientKey=2ddffe14f22f63d33a7f&from=BUE&to=MIA&departureDate=2018-11-17&adults=1&children=1&infants=0&cabinClass=economy&returningDate=2018-11-27')
+var api = request('GET', 'http://navi.api.prd.devtrip.com.ar/flights-api/v1/flights/roundtrips?clientKey=2ddffe14f22f63d33a7f&from=BUE&to=MIA&departureDate=2018-11-17&adults=1&children=1&infants=0&cabinClass=economy&returningDate=2018-11-27')
 
-var result = JSON.parse(res.getBody('utf8'));
+var result = JSON.parse(api.getBody(''));
 
 console.log(result)
+ 
---
+++ Programas completos/Contador de letras.js
@@ -0,0 +1,7 @@
+// Contador de letras
+
+var prompt = require('prompt-sync')()
+
+var input =  prompt('Ingrese una palabra: ')
+
+for(i = 0 ; i )
